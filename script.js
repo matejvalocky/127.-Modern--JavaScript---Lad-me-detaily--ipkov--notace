bb89f8ef989baaaa9bc6e8f70fbe40a47cbbb54c
Matej Valocký <74997007+matejvalocky@users.noreply.github.com>
2024-12-05 21:55:32 +0100

    127. Moderní JavaScript - Ladíme detaily: šipková notace

---
+++ script.js
@@ -0,0 +1,53 @@
+// KLASICKY ZAPIS FUNKCIE:
+// const square = function(num){
+//     return num * num
+// }
+
+
+
+// SKRATENY ZAPIS FUNKCIE
+// const square = (num) => {
+//     return num * num
+// }
+
+
+
+
+// ESTE VIAC SKRATENY ZAPIS FUNKCIE
+const square = (num) => num * num
+
+
+
+// VOLANIE FUNKCIE
+console.log(square(10))
+console.log(square(1))
+console.log(square(2))
+console.log(square(3))
+console.log(square(4))
+console.log(square(5))
+console.log(square(6))
+
+
+
+
+
+
+// 1. napíš šípkovú notáciu pre funkciu súčtu dvoch čísel
+const sum = (num1, num2) => num1 + num2
+
+// volanie funkcie
+console.log(sum(1, 2))
+console.log(sum(3, 4))
+console.log(sum(8, 12))
+
+
+
+// 2. napíš šípkovú notáciu pre funkciu rozdielu dvoch čísel
+const diff = (num1, num2) => num1 - num2
+
+// volanie funkcie
+console.log(diff(10, 5))
+console.log(diff(20, 10))
+console.log(diff(40, 20))
+
+
